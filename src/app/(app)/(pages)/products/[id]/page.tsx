1318a2c6ec0eba610f4217eacac4e9e5a5485017
YeasarArefin <yeasararefin007@gmail.com>
2025-06-27 22:21:41 +0600

    fetch enabled

--- src/app/(app)/(pages)/products/[id]/page.tsx
+++ src/app/(app)/(pages)/products/[id]/page.tsx
@@ -1,33 +1,35 @@
 import Product from "@/components/product/Product";
+import { appUrl } from "@/constants/appUrl";
+import getQuery from "@/lib/queries/getQueries";
 import { Product as ProductType } from "@/types/types";
 
-// // Set the revalidation strategy - 'force-cache' for static generation
-// export const dynamic = 'force-static';
-// export const revalidate = 3600; // Revalidate every hour
+// Set the revalidation strategy - 'force-cache' for static generation
+export const dynamic = 'force-static';
+export const revalidate = 3600; // Revalidate every hour
 
-// export async function generateStaticParams() {
-//     const { success, data } = await getQuery(`${appUrl}api/products`);
+export async function generateStaticParams() {
+    const { success, data } = await getQuery(`${appUrl}api/products`);
 
-//     if (!success || !data) {
-//         return [];
-//     }
+    if (!success || !data) {
+        return [];
+    }
 
-//     return data.map((product: ProductType) => ({
-//         id: product._id,
-//     }));
-// }
+    return data.map((product: ProductType) => ({
+        id: product._id,
+    }));
+}
 
 export default async function Page({ params }: any) {
     const _id = params?.id as string || '';
-    // const { message, success, data } = await getQuery(`${appUrl}api/products?id=${_id}`);
+    const { message, success, data } = await getQuery(`${appUrl}api/products?id=${_id}`);
 
-    // if (!success || !data) {
-    //     return <div className="text-center py-10 text-red-500 font-semibold">Product not found.</div>;
-    // }
+    if (!success || !data) {
+        return <div className="text-center py-10 text-red-500 font-semibold">Product not found.</div>;
+    }
 
-    // const product: ProductType = data;
+    const product: ProductType = data;
 
     return (
-        <Product product={[] as unknown as ProductType} />
+        <Product product={product} />
     );
 }
